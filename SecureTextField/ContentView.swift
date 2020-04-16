2b4c70c244a194bd5db6edb46d0a3b4ff9e67209
Takuya Aso <milanista224@gmail.com>
2020-04-16 21:38:14 +0900

    Initial Commit

---
+++ SecureTextField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SecureTextField
+//
+//  Created by Takuya Aso on 2020/04/16.
+//  Copyright © 2020 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
